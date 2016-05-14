776c26ba7355db486a24442e74a64d0814d002b7
intheblackworld <intheblackworld@gmail.com>
2016-05-14 14:31:30 +0800

    add test form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $('.profile-checkbox p').on("click",function(){
   $(this).children('label').css({'color':'#26a69a'});
 })
+
+function phone(e){
+var phone_num = document.querySelector('.test input').value;
+console.log(phone_num);
+alert(phone_num);
+return false;
+}
