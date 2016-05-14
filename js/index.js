bd4ece7c38dd32046de4d20f3b506791a7f7f9ae
intheblackworld <intheblackworld@gmail.com>
2016-05-14 12:09:29 +0800

    front end first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+
+  function signUp(){
+    swal({   title: "Log In to Continue",
+    html: true,
+    text: "Username: <input type='text'><br>Password: <input type='password'> <br>Password: <input type='password'>",   type: "input",   showCancelButton: true,   closeOnConfirm: false,   animation: "slide-from-top",   inputPlaceholder: "Write something" }, function(inputValue){   if (inputValue === false) return false;      if (inputValue === "") {     swal.showInputError("You need to write something!");     return false   }      swal("Nice!", "You wrote: " + inputValue, "success"); });
+  };
