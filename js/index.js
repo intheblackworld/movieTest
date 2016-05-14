f6cceec3a500749f4f16a23115b7f6209b2cf4a4
intheblackworld <intheblackworld@gmail.com>
2016-05-14 16:40:56 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
+
+
+
 $('.profile-checkbox p').on("click",function(){
   $(this).children('label').css({'color':'#26a69a'});
 })
 
-function phone(e){
-var phone_num = document.querySelector('.test input').value;
-console.log(phone_num);
-alert(phone_num);
-return false;
+
+function phonetests(e){
+  alert(123);
+  var text =$('#phone_number').val();
+  alert(text);
 }
