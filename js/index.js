b9a1e0af7336219e5d69ebdf0794ea345f40f25b
intheblackworld <intheblackworld@gmail.com>
2016-05-17 20:09:50 +0800

    add sweetalert2

--- js/index.js
+++ js/index.js
@@ -13,6 +13,11 @@ function phonetests(e){
     console.log(text);
 }
 
+
+
+
+
+
 $().ready(function(){
 	$('.menu li').on('click',function(){
         var index = $(this).data('index');
@@ -23,5 +28,49 @@ $().ready(function(){
 
   $('.default').filter(function() { return $(this).val() == ""; }).parent('.input-field').find('label').addClass('focus-input');
 
+
+
+  $('.alert_phone').click(function(result){
+
+      swal({
+      title: 'Multiple inputs',
+      html:
+        '<input id="swal-input1" class="swal2-input" type="tel" autofocus>' +
+        '<input id="swal-input2" class="swal2-input" type="password">',
+      preConfirm: function() {
+        return new Promise(function(resolve) {
+          if (result) {
+            resolve([
+              $('#swal-input1').val(),
+              $('#swal-input2').val()
+            ]);
+          }
+        });
+      }
+    }).then(function(result) {
+      if (result) {
+        swal({
+          title: '填寫個人資料',
+          html:
+            '<input id="swal-input3" class="swal2-input" type="text" placeholder="暱稱" autofocus require>',
+            preConfirm: function() {
+              return new Promise(function(resolve) {
+                if (result) {
+                  resolve([
+                    $('#swal-input3').val(),
+                  ]);
+                }
+              });
+            }
+          }).then(function(result) {
+            if (result) {
+              swal('註冊完成');
+            }
+          })
+
+
+      }
+    })
+  });
 
 });
