92abaf09b7d42bcb18429bedd379a98b08597bf4
intheblackworld <intheblackworld@gmail.com>
2016-05-18 11:07:01 +0800

    modify form

--- js/index.js
+++ js/index.js
@@ -31,12 +31,13 @@ $().ready(function(){
 
 
   $('.alert_phone').click(function(result){
-
+// <input id="activation_phone_verification_code_0" type="text" pattern="[0-9]*" class="text-center input-large phone-verification__code-input" placeholder="XXXX" data-reactid=".6.0.$=1$modal.0.0.0.0.1.$=1$verifications.1.$=1$phone_verified.$phone-verification-panel.3.0.1">
       swal({
-      title: 'Multiple inputs',
+      title: '使用電話認證',
       html:
-        '<input id="swal-input1" class="swal2-input" type="tel" autofocus>' +
-        '<input id="swal-input2" class="swal2-input" type="password">',
+        '<input id="swal-input1" class="swal2-input" type="tel" placeholder="電話號碼" autofocus maxlength="10">' +
+        '<input id="swal-input2" class="swal2-input" type="password" placeholder="密碼" >'+
+        '<input id="swal-input2" class="swal2-input" type="text" placeholder="輸入驗證碼" maxlength="6">',
       preConfirm: function() {
         return new Promise(function(resolve) {
           if (result) {
@@ -52,7 +53,12 @@ $().ready(function(){
         swal({
           title: '填寫個人資料',
           html:
-            '<input id="swal-input3" class="swal2-input" type="text" placeholder="暱稱" autofocus require>',
+            '<input id="swal-input3" class="swal2-input" type="text" placeholder="暱稱" autofocus require>'+
+            '<select> <option value="男生">男生</option> <option value="女生">女生</option> </select>'+
+            '<input id="swal-input5" class="swal2-input" type="text" placeholder="年齡" autofocus require>'+
+            '<input id="swal-input6" class="swal2-input" type="text" placeholder="職業" autofocus require>'+
+            '<input id="swal-input7" class="swal2-input" type="text" placeholder="興趣" autofocus require>'+
+            '<input id="swal-input8" class="swal2-input" type="text" placeholder="暱稱" autofocus require>',
             preConfirm: function() {
               return new Promise(function(resolve) {
                 if (result) {
