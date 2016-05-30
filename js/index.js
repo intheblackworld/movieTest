a1df6ddc69919dc22ff46962c90efe88f15767a4
intheblackworld <intheblackworld@gmail.com>
2016-05-30 17:43:13 +0800

    add pickdate

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-$('.profile-checkbox p input').on("click",function(){
-  if ($(this).siblings('label').hasClass('ischeck')) {
-
-      $(this).siblings('label').removeClass('ischeck');
-  }else{$(this).siblings('label').addClass('ischeck');}
-
-})
+// $('.profile-checkbox p input').on("click",function(){
+//   if ($(this).siblings('label').hasClass('ischeck')) {
+//
+//       $(this).siblings('label').removeClass('ischeck');
+//   }else{$(this).siblings('label').addClass('ischeck');}
+// 
+// })
 
 
 function phonetests(e){
@@ -13,12 +13,41 @@ function phonetests(e){
     console.log(text);
 }
 
-
-
+function participate(result) {
+  swal({
+    title: '請選擇有空時間',
+    html: '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_15_wed_night"  /><label for="6_15_wed_night">6/15 禮拜三 晚上 </label></p>'+
+          '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_16_thu_night"  /><label for="6_16_thu_night">6/16 禮拜四 晚上</label></p>'+
+          '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_17_fri_night"  /><label for="6_17_fri_night">6/17 禮拜五 晚上</label></p>'+
+          '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_18_sat_afternoon"  /><label for="6_18_sat_afternoon">6/18 禮拜六 下午</label></p>'+
+          '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_18_sat_night"  /><label for="6_18_sat_night">6/18 禮拜六 晚上</label></p>'+
+          '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_19_sun_afternoon"  /><label for="6_19_sun_afternoon">6/19 禮拜日 下午</label></p>'+
+          '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_19_sun_night"  /><label for="6_19_sun_night">6/19 禮拜日 晚上</label></p>'+
+          '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_20_mon_night"  /><label for="6_20_mon_night">6/20 禮拜一 晚上</label></p>'+
+          '<p class="checktime"><input class="filled-in" name="group1" type="checkbox" id="6_21_tue_night"  /><label for="6_21_tue_night">6/21 禮拜二 晚上</label></p>'+
+          '<h2 style="margin-bottom:24px;">感興趣電影</h2>' +
+          '<p><input type="checkbox" id="1" /><label for="1">電影1</label></p>'+
+          '<p><input type="checkbox" id="2" /><label for="2">電影2</label></p>'+
+          '<p><input type="checkbox" id="3" /><label for="3">電影3</label></p>'+
+          '<p><input type="checkbox" id="4" /><label for="4">電影4</label></p>'+
+          '<p><input type="checkbox" id="5" /><label for="5">電影5</label></p>'+
+          '<p><input type="checkbox" id="6" /><label for="6">電影6</label></p>',
+  })
+  $('.swal2-modal').css({'top':'48%','height': '100%','max-height': '100%'});
+}
 
 
 
 $().ready(function(){
+
+
+  var limit = 4 ;
+  $('.profile-checkbox p input').on('change', function(evt) {
+   if($('.profile-checkbox p input:checked').length >= limit) {
+       this.checked = false;
+   }
+});
+
 	$('.menu li').on('click',function(){
         var index = $(this).data('index');
         $('.'+index).addClass('active');
@@ -39,6 +68,8 @@ $().ready(function(){
     $('.swal2-modal').css({'top':'70%','height': '60%'});
     $('.bank').click(function(){$('.hide-text').fadeIn(300);})
   })
+
+
 
 
   $('.alert_phone').click(function(result){
@@ -70,11 +101,9 @@ $().ready(function(){
           title: '填寫個人資料',
           html:
             '<label>暱稱</label><input id="swal-input3" class="swal2-input" type="text" placeholder="暱稱" autofocus require>'+
-            '<select> <option value="男生">男生</option> <option value="女生">女生</option> </select>'+
-            '<input id="swal-input5" class="swal2-input" type="text" placeholder="年齡" autofocus require>'+
-            '<select> <option value="男生">男生</option> <option value="女生">女生</option> </select>'+
-            '<input id="swal-input7" class="swal2-input" type="text" placeholder="興趣" autofocus require>'+
-            '<input id="swal-input8" class="swal2-input" type="text" placeholder="暱稱" autofocus require>',
+            '<label>性別</label><select> <option value="男生">男生</option> <option value="女生">女生</option> </select>'+
+            '<label>年齡</label><input id="swal-input5" class="swal2-input" type="text" placeholder="年齡" autofocus require>'+
+            '<label>興趣</label><input id="swal-input7" class="swal2-input" type="text" placeholder="興趣" autofocus require>',
             preConfirm: function() {
               return new Promise(function(resolve) {
                 if (result) {
@@ -86,7 +115,7 @@ $().ready(function(){
             }
           }).then(function(result) {
             if (result) {
-              swal('註冊完成');
+              swal('臉書登入');
             }
           })
 
