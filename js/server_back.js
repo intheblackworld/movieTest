5a7ea5aa3fb73b5f23def5d3440c2439a9b6663d
neversaynever0502 <neversaynever0502@gmail.com>
2016-06-01 23:57:41 +0800

    後台back.html中user完成

--- js/server_back.js
+++ js/server_back.js
@@ -1,46 +1,111 @@
-	var myFirebaseRef = new Firebase("https://letusmovie.firebaseio.com/");
-	var usersRef = myFirebaseRef.child("users");
-	var activitysRef = myFirebaseRef.child("activitys")
+var config = {
+                apiKey: "AIzaSyAuzEl07MHtqY68GScLEdGen7Pwxe4AsTY",
+                authDomain: "project-1685736578378459097.firebaseapp.com",
+                databaseURL: "https://project-1685736578378459097.firebaseio.com",
+                storageBucket: "project-1685736578378459097.appspot.com",
+              };
+ firebase.initializeApp(config);
 
-	var allpeople = ""
-	inputText=prompt('請輸入密碼');
-	if (inputText != "k"){
-		   window.reload();
-        
-	}
 
-	usersRef.on("value",function(all){
-		
-		console.log(Object.keys(all.val())[0]);
-		for (i=0;i<all.numChildren();i++){
-			allpeople = allpeople + Object.keys(all.val())[i] + "<br>";
-			document.getElementById("allusers").innerHTML = allpeople
-		}
-	});
+ function signUp(){
+                   var auth = firebase.auth();
+                   var provider = new firebase.auth.FacebookAuthProvider();
+                   auth.signInWithPopup(provider).then(function(result) {
+                   var uid = result.user.uid;
+                   var displayName = result.user.displayName;
+                   var photoURL = result.user.photoURL;
+        			  var email = result.user.email;
+
+                     firebase.database().ref('users/'+uid).update({
+                                      uid: uid,
+                                     displayName: displayName,
+                                     photoURL: photoURL,
+                                     email:email,
+                   //                   lineID:' ',
+ 						            // phone:' ',
+ 						            // nickname:'',
+ 						            // gender:'',
+ 						            // age:'',
+ 						            // description:[],
+ 						            // profession:'',
+ 						            // interests:'',
+ 						            // faceScore:''
+
+                       });
+                 }).catch(function(error) {
+                   // An error occurred
+                 });
+                 }
+// 臉書判斷是否登入
+var auth = firebase.auth();
+			var name;
+			var uid;
+			auth.onAuthStateChanged(function(user) {
+				if (user) {
+					uid = user.uid;
+					var name = user.displayName;
+					var photoURL = user.photoURL;
+					console.log("uid :"+uid+" User Login");
+					console.log("name:"+name);
+					console.log("photo網址"+photoURL);
+					//index.html
+					$('.avatar, .user_name, .check_date, .logout').show();
+					$('.fb_login').hide();
+					$('#pic_my').attr("src",photoURL);
+					$('.user_name p').text(name+ "，歡迎回來");
+
+					//admin_profile
+				} else {
+						$('.fb_login').show();
+						console.log("沒登入");
+					// User logged out
+				}
+			});
+
+
+			firebase.database().ref('users').once("value").then(function(all){
+						
+						var userUID;
+
+                    	new Vue({
+                          el: '#app3',
+                          data: {
+                          	users:all.val(),
+                          	query:'',
+                          	faceScore:''
+                          	},
+                          methods:{
+                              showDetail: function(x){
+                                  userUID = x.uid;
+                                  document.getElementById('detail').className = 'show';
+                                  // document.getElementById('detail').innerHTML = x.displayName;
+                                  this.faceScore = x.faceScore;
+                                  console.log(userUID)
+                              },
+                              updateUser: function(){
+                              	console.log(userUID);
+                              	firebase.database().ref('users/'+userUID).update({
+									faceScore: this.faceScore
+								}).catch(function(error) {
+				                  // An error occurred
+				                });
+				                document.getElementById('detail').className = 'hide';
+				                window.location.reload();
+                              }
+                          }
+                        });
+                });
+
+
+
+			function signOut(){
+
+			    	firebase.auth().signOut().then(function() {
+			   // Sign-out successful.
+			 }, function(error) {
+			   // An error happened.
+			 });
+
 			
-	// function whoJoinThatActivity(){
-					activitysRef.on("value",function(all){	
-						var acts = [];
-						console.log(Object.keys(all.val())[0]);
-						for (i=0;i<all.numChildren();i++){
-							date = Object.keys(all.val())[i] ;
 
-							var activityUserRef = "";
-							activityUserRef = activitysRef.child(date);
-							var allpeople = ""
-							activityUserRef.on("value",function(all){
-								for (i=0;i<all.numChildren();i++){
-									allpeople = allpeople + Object.keys(all.val())[i]+" " +all.val()[Object.keys(all.val())[i]].mostLikeMovie + "<br>";
-									acts.push(allpeople);
-								}
-						    });
-						    }
-						    var x = ""
-						for(i=0;i<acts.length;i++){
-							x=x+"第"+(i+1)+"場活動"+"<br>"+acts[i]+"<br>";
-
-						document.getElementById("activity").innerHTML = x;
-						}
-					});
-			// }
-
+	    }
